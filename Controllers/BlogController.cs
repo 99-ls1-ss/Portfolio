857655d958e282465d1a24989a3be2e19e1c63c2
99-ls1-ss <branpayne69@gmail.com>
2015-10-07 16:58:33 -0400

    Working on adding a counter for the blog

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -64,9 +64,7 @@ namespace Portfolio.Controllers {
                 if (db.Posts.Any(p => p.Slug == Slug)) {
                     ModelState.AddModelError("Title", "Your Title must be unique.");
                     return View(blogPost);
-                }
-
-                
+                }                
 
                 if (image != null) {
                     //relative server path
@@ -110,8 +108,31 @@ namespace Portfolio.Controllers {
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Created,Updated,Title,Slug,Body,MediaURL,IsPublished,IsLoggedIn,AccessLevel")] BlogPost blogPost) {
+        public ActionResult Edit([Bind(Include = "Id,Created,Updated,Title,Slug,Body,MediaURL,IsPublished,IsLoggedIn,AccessLevel")] BlogPost blogPost, HttpPostedFileBase image) {
+            if (image != null && image.ContentLength > 0) {
+                //Check the file name to make sure it's a image
+                var ext = Path.GetExtension(image.FileName).ToLower();
+
+                if (ext != ".png" && ext != ".jpg" && ext != ".jpeg" && ext != ".gif" && ext != ".bmp")
+                    ModelState.AddModelError("image", "Invalid Image Type.");
+            }
+
             if (ModelState.IsValid) {
+
+                if (image != null) {
+                    //relative server path
+                    var filePath = "/Uploads/";
+
+                    //Path on physical drive on server
+                    var absPath = Server.MapPath("~" + filePath);
+
+                    //Media URL for relative path
+                    blogPost.MediaURL = filePath + image.FileName;
+
+                    //Save image
+                    image.SaveAs(Path.Combine(absPath, image.FileName));
+                }
+
                 db.Entry(blogPost).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
